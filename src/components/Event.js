515e60ff1df6cafbc80b283b76db3a4ab7632fb6
Kari <kari.lynn.dreyer@gmail.com>
2024-10-12 09:09:18 +0200

    Add functionality for feature 2 scenarios

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,11 +1,21 @@
 // src/components/Event.js
+import React, { useState } from 'react';
 
 const Event = ({ event }) => { // Receive the event prop from the parent component
+
+    const [showDetails, setShowDetails] = useState(false); // Initialize the showDetails state variable to false
+    
     return (
-        <li>
+        <li className="Event">
             <h1>{event.summary}</h1>
             <p>{event.created}</p>
             <p>{event.location}</p>
+            <button className="detail-btn" onClick={()=>setShowDetails(!showDetails)}>
+                { showDetails ? "Hide details" : "Show details"}
+            </button>
+            {
+                showDetails ?  <p role="description" className="details">{event.description}</p> : <></>
+            }
         </li>
     );
 }
