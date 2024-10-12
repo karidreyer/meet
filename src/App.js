3a280e27051b081cdcdeccf556223aad3f8fa80b
Kari <kari.lynn.dreyer@gmail.com>
2024-10-12 10:24:52 +0200

    Add NumberOfEvents component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
+import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <CitySearch />
+      <NumberOfEvents />
       <EventList />
     </div>
   );
