89fedf399fe1d8f1038101285ad1c5f040468f9e
Kari <kari.lynn.dreyer@gmail.com>
2024-10-12 14:45:06 +0200

    Develop component features for feature 3, scenarios 1-2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 // src/App.js
 
+import React, { useState } from 'react';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 import './App.css';
 
 function App() {
+
+  const [numberOfEvents, setNumberOfEvents] = useState(32); // Set default number of events to 32
+
   return (
     <div className="App">
       <CitySearch />
-      <NumberOfEvents />
-      <EventList />
+      <NumberOfEvents numberOfEvents={numberOfEvents} setNumberOfEvents={setNumberOfEvents} />
+      <EventList numberOfEvents={numberOfEvents} />
     </div>
   );
 }
--- src/components/NumberOfEvents.js
+++ src/components/NumberOfEvents.js
@@ -1,14 +1,29 @@
 // src/components/NumberOfEvents.js
 
-const NumberOfEvents = ({ numberOfEvents, setNumberOfEvents }) => {
+import React, { useState, useEffect } from 'react';
+
+const NumberOfEvents = ({ numberOfEvents = 32, setNumberOfEvents }) => {
+    const [inputValue, setInputValue] = useState(numberOfEvents);
+
+    // Update inputValue when numberOfEvents changes
+    useEffect(() => {
+        setInputValue(numberOfEvents);
+    }, [numberOfEvents]); 
+
+    const handleInputChange = (e) => {
+        const value = e.target.value;
+        setInputValue(value); // Update local inputValue state
+        setNumberOfEvents(value); // Pass the new value back to App component
+    };
+
     return (
         <div id="number-of-events">
             <label htmlFor="number-of-events-input">Number of Events:</label>
             <input
                 type="number"
                 id="number-of-events-input"
-                value={numberOfEvents}
-                onChange={(e) => setNumberOfEvents(e.target.value)}
+                value={inputValue} // Controlled by local inputValue state
+                onChange={handleInputChange} // Updates inputValue and parent state on change
             />
         </div>
     );
