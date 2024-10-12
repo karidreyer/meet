af3e0252e8cd7636a9c78c7accfc7a3a93427948
Kari <kari.lynn.dreyer@gmail.com>
2024-10-12 14:43:20 +0200

    Create tests for feature 3, scenarios 1-2

---
+++ src/__tests__/NumberOfEvents.test.js
@@ -0,0 +1,35 @@
+// src/__tests__/NumberOfEvents.test.js
+
+import NumberOfEvents from '../components/NumberOfEvents';
+import { render } from '@testing-library/react';
+import userEvent from "@testing-library/user-event";
+import { getEvents } from '../api';
+
+describe('<NumberOfEvents /> component', () => {
+    let NumberOfEventsComponent;
+
+    beforeEach(() => {
+        NumberOfEventsComponent = render(<NumberOfEvents setNumberOfEvents={() => {}}/>);
+    });
+
+    test('component contains a textbox for input', () => {
+        const numberOfEventsTextBox = NumberOfEventsComponent.queryByRole('spinbutton'); // "spinbutton" is the role for input type="number"
+        expect(numberOfEventsTextBox).toBeInTheDocument();
+    });
+
+    test('ensure that the default number of events is 32', () => {
+        const numberOfEventsTextBox = NumberOfEventsComponent.queryByRole('spinbutton'); // "spinbutton" is the role for input type="number"
+        expect(numberOfEventsTextBox).toHaveValue(32);
+    });
+
+    test('ensure that the number of events can be changed' , async () => {
+        const numberOfEventsTextBox = NumberOfEventsComponent.queryByRole('spinbutton'); // "spinbutton" is the role for input type="number"
+        
+        const user = userEvent.setup();
+        await user.type(numberOfEventsTextBox, '{backspace}{backspace}10');
+
+        const allEvents = await getEvents();
+        NumberOfEventsComponent.rerender(<NumberOfEvents />);
+        expect(numberOfEventsTextBox).toHaveValue(10);
+    });
+});
