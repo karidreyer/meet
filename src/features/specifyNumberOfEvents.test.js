36612d438a85d3af2b8eb6ba16d6c2d6f4720788
Kari <kari.lynn.dreyer@gmail.com>
2024-10-28 19:10:53 +0100

    Develop acceptance tests for Specify Number of Events feature

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -1,37 +1,59 @@
 import { loadFeature, defineFeature } from 'jest-cucumber';
+import { render, waitFor, within } from '@testing-library/react';
+import App from '../App';
+import userEvent from '@testing-library/user-event';
 
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 defineFeature(feature, test => {
-    test('When user hasn’t specified a number, 32 events are shown by default', ({ given, when, then }) => {
-        given('the main page is open', () => {
 
+    test('When user hasn’t specified a number, 32 events are shown by default', ({ given, when, then }) => {
+        let AppComponent;
+        given('the main page is open', () => {
+            // Render App component
+            AppComponent = render(<App />);
         });
 
         when('the user has not set a specific number of events to display', () => {
-
+            // No action required
         });
 
-        then(/^the user should see (\d+) events displayed$/, (arg0) => {
+        then(/^the user should see (\d+) events displayed$/, async () => {
+            // Check that the default number of events (32) is displayed
+            const AppDOM = AppComponent.container.firstChild;
+            const EventListDOM = AppDOM.querySelector('#event-list');
 
+            await waitFor(() => {
+                const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+                expect(EventListItems.length).toBe(32);
+            });
         });
     });
 
-    test('User can change the number of events displayed', ({ given, when, then, and }) => {
+    test('User can change the number of events displayed', ({ given, when, then }) => {
+        let AppComponent;
+        let numberOfEventsTextBox;
         given('the main page is open', () => {
-
+            // Render the App component
+            AppComponent = render(<App />);
         });
 
-        when('the user selects the option to show a specific number of events', () => {
-
+        when('the user selects the option to show a specific number of events', async () => {
+            // Simulate user changing the number of events
+            numberOfEventsTextBox = AppComponent.getByRole('spinbutton');
+        
+            const user = userEvent.setup();
+            await user.clear(numberOfEventsTextBox);
+            await user.type(numberOfEventsTextBox, '10');
         });
 
-        then('the page should display that number of events', () => {
-
-        });
-
-        and('any additional events should be accessible through pagination or scrolling', () => {
-
+        then('the page should display that number of events', async () => {
+            // Check that the specified number of events is displayed
+            const EventListDOM = AppComponent.container.querySelector('#event-list');
+            await waitFor(() => {
+                const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+                expect(EventListItems.length).toBe(10);
+            });
         });
     });
 });
